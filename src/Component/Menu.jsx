34c16f6247033b8e2dd0d9c1428c5340eb8eef9f
harshilpec17 <harshilsuthar@hotmail.com>
2023-06-09 05:45:18 -0400

    Update the API response

--- src/Component/Menu.jsx
+++ src/Component/Menu.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Shimmer from "./ShimmerComponent/ShimmerBody";
-import { Params, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { RES_MENU } from "../utils/constant";
 
 const menu = () => {
