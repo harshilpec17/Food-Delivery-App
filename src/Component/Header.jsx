5e7c0790168735046228025ea39f9eaa9dff3ff8
harshilpec17 <harshilsuthar@hotmail.com>
2023-06-19 14:02:52 -0400

    make headers and menu a mobile responsive

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -5,29 +5,35 @@ const Header = () => {
   return (
     <>
       <div>
-        <div className="NavWrapper">
-          <div className="logo">
-            <img src={LOGO_URL} alt="logo" className="CompanyLogo" />
+        <div className="NavWrapper flex flex-row justify-between items-center px-2 md:px-10 py-2 ">
+          <div className="logo ">
+            <img
+              src={LOGO_URL}
+              alt="logo"
+              className="CompanyLogo w-[80px] p-2.5"
+            />
           </div>
-          <div className="NavItems">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/offers">Offers</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact Us </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="Buttons">
-            <button className="btn">Sign Up</button>
-            <button className="btn">Login</button>
+          <div className="md:flex md:justify-between items-center gap-28">
+            <div className="NavItems">
+              <ul className="flex text-[16px] md:text-[20px] justify-between px-3 md:gap-24">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About Us</Link>
+                </li>
+                <li>
+                  <Link to="/offers">Offers</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact Us </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="Buttons flex justify-between">
+              <button className="btn w-36">Sign Up</button>
+              <button className="btn w-36">Login</button>
+            </div>
           </div>
         </div>
       </div>
--- src/Component/Menu.jsx
+++ src/Component/Menu.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import Shimmer from "./ShimmerComponent/ShimmerBody";
 import { useParams } from "react-router-dom";
 import { RES_MENU } from "../utils/constant";
-import resName from "../utils/mockName";
+import { FaClock } from "react-icons/fa";
+import { BsFillStarFill } from "react-icons/bs";
 
 import fakeJson from "./fakeData";
 
@@ -17,13 +18,28 @@ const menu = () => {
   const fetchMenu = async () => {
     const data = await fetch(RES_MENU + id);
     const json = await data.json();
+    console.log(json);
     setResInfo(json.data || fakeJson.data);
   };
 
   if (resInfo === null) return <Shimmer />;
 
-  const { name, cuisines, costForTwoMessage } =
-    resInfo?.cards[0]?.card?.card?.info;
+  // for the name, cuisines, costForTwoMessage, avgRating, totalRatingsString, locality information for the restaurants
+
+  const {
+    name,
+    cuisines,
+    costForTwoMessage,
+    avgRating,
+    totalRatingsString,
+    locality,
+  } = resInfo?.cards[0]?.card?.card?.info;
+
+  // for the deliveryTime information on the menu
+
+  const { deliveryTime } = resInfo?.cards[0]?.card?.card?.info?.sla;
+
+  // for the menu dishes, according to the open time of place, it will display the available dishes
 
   const { itemCards } =
     resInfo?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
@@ -34,10 +50,44 @@ const menu = () => {
 
   return (
     <div className=" text-white content-center bg-gray-600">
-      <h1>{name}</h1>
-      <h2>{cuisines.join(", ")}</h2>
-      <h2>{costForTwoMessage}</h2>
-      <div>
+      <div className="basicInformation flex md:flex-row justify-between px-6 md:px-16 py-6 border-b-2 ">
+        <div>
+          <h1 className="font-bold md:text-[28px] ">{name}</h1>
+          <p className="md:text-[16px]">{cuisines.join(", ")}</p>
+          <p className="md:text-[16px]">{locality}</p>
+        </div>
+        <div>
+          <div className="border-2 rounded text-center p-1">
+            <p className="border-b-2 gap-1 flex justify-center py-1">
+              <BsFillStarFill />
+              {avgRating}
+            </p>
+            <p className="pt-1">{totalRatingsString}</p>
+          </div>
+        </div>
+      </div>
+      <div className="time costForTwo border-b-2 flex justify-between px-6 md:px-16 py-4 gap-20">
+        <p className="md:text-[16px] flex gap-1 items-center">
+          <FaClock /> {deliveryTime} Mins
+        </p>
+        <p className="md:text-[16px]">{costForTwoMessage}</p>
+      </div>
+      <div className="discount px-6 md:px-16 py-4 w-max">
+        <div className="discount px-4 py-2 card flex flex-col border rounded">
+          <div>
+            <h2 className="text-center">Header</h2>
+          </div>
+          <div className="flex flex-row gap-2">
+            <p>code</p>
+            <p> | description</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="menu">
+        <h1>
+          <span>( )</span>
+        </h1>
         <ul>
           {itemCards.map((item) => (
             <li key={item.card.info.id}>
@@ -47,7 +97,6 @@ const menu = () => {
               }`}
             </li>
           ))}
-          <li>nachoss</li>
         </ul>
       </div>
     </div>
