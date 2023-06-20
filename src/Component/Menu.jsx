845992296b5466da22898849706f2bcb98751dea
harshilpec17 <harshilsuthar@hotmail.com>
2023-06-20 13:07:30 -0400

    Added a discount dynamic card from API

--- src/Component/Menu.jsx
+++ src/Component/Menu.jsx
@@ -9,7 +9,7 @@ import fakeJson from "./fakeData";
 
 const menu = () => {
   const [resInfo, setResInfo] = useState(null);
-  const [discount] = useState(null);
+  const [discount, setDiscount] = useState(null);
 
   const { id } = useParams("112620245");
   useEffect(() => {
@@ -74,25 +74,27 @@ const menu = () => {
         </p>
         <p className="md:text-[16px]">{costForTwoMessage}</p>
       </div>
-      <div className="discount px-6 md:px-16 py-4 w-max">
+      <div className="discountWrapper md:px-14 flex flex-col md:flex-row items-left">
         {discount?.cards[1]?.card?.card?.gridElements?.infoWithStyle.offers.map(
-          (x) => (props = { x })
+          (x) => (
+            <div className="discount px-6 md:px-2 py-3 w-max">
+              <div className="px-4 py-2 card border rounded">
+                <div>
+                  <h2 className="text-center">{x.info.header}</h2>
+                </div>
+                <div className="flex flex-row gap-2">
+                  <p>{x.info.couponCode}</p>
+                  <p> | {x.info.description}</p>
+                </div>
+              </div>
+            </div>
+          )
         )}
-
-        <div className="discount px-4 py-2 card flex flex-col border rounded">
-          <div>
-            <h2 className="text-center">{props.info.header}</h2>
-          </div>
-          <div className="flex flex-row gap-2">
-            <p>code</p>
-            <p> | description</p>
-          </div>
-        </div>
       </div>
 
       <div className="menu">
         <h1>
-          <span>( )</span>
+          <span>Recomended ( )</span>
         </h1>
         <ul>
           {itemCards.map((item) => (
